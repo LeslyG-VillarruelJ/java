1192e6a8b810dc088237eb4cf5f91e30bca75043
LeslyG-VillarruelJ <leslyvillarruel90@gmail.com>
2024-03-12 10:17:57 -0500

    Metodo multiplicar, clase calculadora

--- Clases/src/com/krakedev/Calculadora.java
+++ Clases/src/com/krakedev/Calculadora.java
@@ -10,4 +10,9 @@ public class Calculadora {
 		int resultado = valor1 - valor2;
 		return resultado;
 	}
+	
+	public double multiplicar(double valor1, double valor2) {
+		double resultado = valor1 * valor2;
+		return resultado;
+	}
 }
