a5b1a6f6129c5926fcb7eb894a4c5c08f42f8bc4
LeslyG-VillarruelJ <leslyvillarruel90@gmail.com>
2024-03-12 12:07:13 -0500

    Invocar metodos creados en clase Calculadora

--- Clases/src/com/krakedev/TestCalculadora.java
+++ Clases/src/com/krakedev/TestCalculadora.java
@@ -7,10 +7,22 @@ public class TestCalculadora {
 		Calculadora calcu = new Calculadora();
 		int suma;
 		int resta;
+		double multiplicacion;
+		double division;
+		double promedio;
 		suma = calcu.sumar(10, 8);
 		resta = calcu.restar(21, 7);
 		System.out.println("Suma: " + suma);
 		System.out.println("Resta: " + resta);
+		// other methods
+		multiplicacion = calcu.multiplicar(7, 9);
+		division = calcu.dividir(18, 5);
+		promedio = calcu.promediar(9, 7, 6.5);
+		// Show the results
+		System.out.println("Multiplicación: " + multiplicacion);
+		System.out.println("Division: " + division);
+		System.out.println("Promedio: " + promedio);
+		calcu.mostrarResultado();
 	}
 
 }
