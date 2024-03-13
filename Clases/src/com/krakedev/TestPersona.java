a3264e4442666e252a5c7c161e2dcc6622c1fe14
LeslyG-VillarruelJ <leslyvillarruel90@gmail.com>
2024-03-13 10:58:49 -0500

    Constructores

--- Clases/src/com/krakedev/TestPersona.java
+++ Clases/src/com/krakedev/TestPersona.java
@@ -7,6 +7,8 @@ public class TestPersona {
 		Persona person1; //Declared a variable of type
 		person1 = new Persona(); // Instance the variable (object)
 		Persona person2 = new Persona();
+		Persona person3 = new Persona("Pablo");
+		Persona person4 = new Persona("Rocio", 43, 1.60);
 		//Accede to the attributes
 		System.out.println("nombre: " + person1.getNombre());
 		System.out.println("edad: " + person1.getEdad());
@@ -26,6 +28,12 @@ public class TestPersona {
 		System.out.println("************");
 		System.out.println("person1.nombre" + person1.getNombre());
 		System.out.println("person2.nombre" + person2.getNombre());
+		System.out.println("person3.nombre: " + person3.getNombre());
+		
+		System.out.println("-------------------");
+		System.out.println("nombre: " + person4.getNombre());
+		System.out.println("edad: " + person4.getEdad());
+		System.out.println("estatura: " + person4.getEstatura());
 	}
 
 }
