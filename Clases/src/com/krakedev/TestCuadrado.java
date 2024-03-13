1c2dfd14fd33055df65f70fa5be8683365703c50
LeslyG-VillarruelJ <leslyvillarruel90@gmail.com>
2024-03-12 21:29:01 -0500

    Clase Cuadrado con metodos y atributos, testeado

---
+++ Clases/src/com/krakedev/TestCuadrado.java
@@ -0,0 +1,25 @@
+package com.krakedev;
+
+import com.krakedev.Cuadrado;
+
+public class TestCuadrado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cuadrado c1 = new Cuadrado();
+		Cuadrado c2 = new Cuadrado();
+		Cuadrado c3 = new Cuadrado();
+		// Modify attributes
+		c1.lado = 14.5;
+		c2.lado = 8.9;
+		c3.lado = 18.3;
+		// Get area and perimeter
+		double area1 = c1.calcularArea();
+		double area2 = c2.calcularArea();
+		double area3 = c3.calcularArea();
+		double perimetro1 = c1.calcularPerimetro();
+		double perimetro2 = c2.calcularPerimetro();
+		double perimetro3 = c3.calcularPerimetro();
+	}
+
+}
